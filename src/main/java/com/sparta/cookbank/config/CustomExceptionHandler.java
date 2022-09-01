5dbc307f29698c58b5fc3b113c72bf9b4b0c1365
jobmania <xzxz7003@naver.com>
2022-09-01 09:52:39 +0900

    feature/ update 전역 예외처리 기능 추가.

--- src/main/java/com/sparta/cookbank/config/CustomExceptionHandler.java
+++ src/main/java/com/sparta/cookbank/config/CustomExceptionHandler.java
@@ -3,16 +3,19 @@ package com.sparta.cookbank.config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.cookbank.ResponseDto;
 import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice     // 글로벌로 적용된다.
 public class CustomExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseDto<?> handleIllegalArgumentException(IllegalArgumentException exception) {
         String errorMessage = exception.getMessage();
         return ResponseDto.fail("400",errorMessage);
     }
-
+    
     @ExceptionHandler(JsonProcessingException.class)
     public ResponseDto<?> handleJsonProcessingException(JsonProcessingException exception){
         String errorMessage = "readTree Fail" + exception.getMessage();
