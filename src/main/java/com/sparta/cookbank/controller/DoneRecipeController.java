f41ffd296feecc887906da208fcc10e72c0daf81
Rzreo <zuzu5313@naver.com>
2022-09-01 20:17:43 +0900

    만든 레시피 저장

---
+++ src/main/java/com/sparta/cookbank/controller/DoneRecipeController.java
@@ -0,0 +1,23 @@
+package com.sparta.cookbank.controller;
+
+import com.sparta.cookbank.ResponseDto;
+import com.sparta.cookbank.domain.DoneRecipe.dto.DoneRecipeRequestDto;
+import com.sparta.cookbank.service.DoneRecipeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class DoneRecipeController {
+    private final DoneRecipeService doneRecipeService;
+
+
+    @PostMapping("/api/recipe/finish")
+    public ResponseDto<?> RecipeDone(@RequestParam Long id, @RequestBody DoneRecipeRequestDto requestDto){
+        doneRecipeService.UsedIngredient(id,requestDto);
+        return ResponseDto.success(id,"나의 레시피 저장소에 저장되었습니다.");
+    }
+}
