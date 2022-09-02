9c0bd44123d8b88a1c727c7e3dd617c03d930df5
Rzreo <98319611+Rzreo@users.noreply.github.com>
2022-09-02 09:39:18 +0900

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/*SNAPSHOT.jar.jar"
+JAR_FILE="$PROJECT_ROOT/build/libs/*SNAPSHOT.jar.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
