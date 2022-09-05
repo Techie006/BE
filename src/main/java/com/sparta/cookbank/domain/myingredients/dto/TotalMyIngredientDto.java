0a187a7a3b58de27bb4219ea96e99b0b4988bbb5
jobmania <xzxz7003@naver.com>
2022-09-05 20:09:30 +0900

    [손원철] <feat> 재료 전체조회(response 수정)

---
+++ src/main/java/com/sparta/cookbank/domain/myingredients/dto/TotalMyIngredientDto.java
@@ -0,0 +1,21 @@
+package com.sparta.cookbank.domain.myingredients.dto;
+
+import com.sparta.cookbank.domain.Storage;
+import com.sparta.cookbank.domain.ingredient.enums.FoodCategory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TotalMyIngredientDto {
+    private Long id;
+    private String food_name;
+    private FoodCategory group_name;
+    private String in_date;
+    private String d_date;
+    private Storage category;
+}
