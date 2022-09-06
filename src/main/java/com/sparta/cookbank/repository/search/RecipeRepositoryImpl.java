50437cd77bd4d30a7e86884bcad7e5b78804522d
choonge07 <choonge_@naver.com>
2022-09-06 10:57:46 +0900

    <choonge07>[fix] 코드정리

--- src/main/java/com/sparta/cookbank/repository/search/RecipeRepositoryImpl.java
+++ src/main/java/com/sparta/cookbank/repository/search/RecipeRepositoryImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
