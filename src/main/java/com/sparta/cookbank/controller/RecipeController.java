128ddd3cdadd7f8cf10128a3bd639b3f9431d9b4
Rzreo <zuzu5313@naver.com>
2022-09-17 11:07:43 +0900

    get을 post로 변경

--- src/main/java/com/sparta/cookbank/controller/RecipeController.java
+++ src/main/java/com/sparta/cookbank/controller/RecipeController.java
@@ -14,7 +14,7 @@ public class RecipeController {
     private final RecipeService recipeService;
 
     // 추천 레시피 조회
-    @GetMapping("/api/recipes/recommend")
+    @PostMapping("/api/recipes/recommend")
     public ResponseDto<?> getRecommendRecipe(@RequestBody RecipeRecommendRequestDto requestDto) {
         RecipeRecommendResultResponseDto resultResponseDto = recipeService.getRecommendRecipe(requestDto);
         return ResponseDto.success(resultResponseDto,"추천레시피 제공에 성공하였습니다.");
@@ -35,7 +35,7 @@ public class RecipeController {
     }
 
     // 레시피 검색
-    @GetMapping("/api/recipes/search")
+    @PostMapping("/api/recipes/search")
     public ResponseDto<?> searchRecipe(@RequestBody RecipeSearchRequestDto searchRequestDto,
                                        @PageableDefault(size = 5) Pageable pageable) {
         RecipeResponseDto ResponseDtoPage = recipeService.searchRecipe(searchRequestDto,pageable);
