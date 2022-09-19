c9ce57f8a17acc10805b91f59be09e08676113b2
Rzreo <zuzu5313@naver.com>
2022-09-19 12:22:14 +0900

    [Rzreo] <feat> 테스트 채팅

--- src/main/java/com/sparta/cookbank/controller/StompChatController.java
+++ src/main/java/com/sparta/cookbank/controller/StompChatController.java
@@ -2,6 +2,7 @@ package com.sparta.cookbank.controller;
 
 import com.sparta.cookbank.domain.chat.dto.ChatDto;
 import com.sparta.cookbank.domain.chat.dto.ChatResponseDto;
+import com.sparta.cookbank.domain.chat.dto.ChatTest;
 import com.sparta.cookbank.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,12 +24,12 @@ public class StompChatController {
     //"/pub/chat/enter"
     //메세지 받았을 때
     @MessageMapping(value = "/chat/room/{roomId}")
-    public void message(@DestinationVariable String class_id, ChatDto message) {
+    public void message(@DestinationVariable String class_id, ChatTest message) {
         //채팅 저장
-        ChatResponseDto responseDto = chatService.saveChat(Long.parseLong(class_id), message);
+        //ChatResponseDto responseDto = chatService.saveChat(Long.parseLong(class_id), message);
         log.info("pub success " + message.getMessage());
         //채팅방에 정보 전달
-        template.convertAndSend("/sub/chat/room/" + class_id, responseDto);
+        template.convertAndSend("/sub/chat/room/" + class_id, message);
     }
 
 }
