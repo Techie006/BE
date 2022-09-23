00e98ac0d809732985757d11e37d6d70e15b042a
jobmania <xzxz7003@naver.com>
2022-09-23 10:19:40 +0900

    [손원철]<fix>: 프론트 요청으로 재료 자동검색 requestbody에서 reqeustparm 으로 수정.#59

--- src/main/java/com/sparta/cookbank/repository/IngredientsRepository.java
+++ src/main/java/com/sparta/cookbank/repository/IngredientsRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface IngredientsRepository extends JpaRepository<Ingredient, Long> {
 
     Page<Ingredient> findAllByFoodNameIsContaining(String food_name, Pageable pageable);  // 포함된 글자가 맨처음으로 오는게 맨위로 할려면?
-    List<Ingredient> findAllByFoodNameIsContainingOrderByFoodName(String food_name);
+    List<Ingredient> findAllByFoodNameIsContainingOrderByMarkName(String food_name);
 }
