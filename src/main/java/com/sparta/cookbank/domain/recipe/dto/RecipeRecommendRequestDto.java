3360e0a4a69991d036e3df9a091dd971ba7741ff
choonge07 <choonge_@naver.com>
2022-09-30 16:13:36 +0900

    [choonge07]<test> 테스트 코드 수정

--- src/main/java/com/sparta/cookbank/domain/recipe/dto/RecipeRecommendRequestDto.java
+++ src/main/java/com/sparta/cookbank/domain/recipe/dto/RecipeRecommendRequestDto.java
@@ -1,10 +1,7 @@
 package com.sparta.cookbank.domain.recipe.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +10,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @Getter
+@EqualsAndHashCode
 public class RecipeRecommendRequestDto {
 
     // 추천 레시피의 메인 재료
