059915d333b8b0ff0aafdc39b258c7fb6074f044
choonge07 <choonge_@naver.com>
2022-10-01 17:45:46 +0900

    [choonge07]<fix> pageable 수정

--- src/main/java/com/sparta/cookbank/config/WebConfig.java
+++ src/main/java/com/sparta/cookbank/config/WebConfig.java
@@ -1,2 +1,19 @@
-package com.sparta.cookbank.config;public class WebConfig {
+package com.sparta.cookbank.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        PageableHandlerMethodArgumentResolver pageableArgumentResolver = new PageableHandlerMethodArgumentResolver();
+        pageableArgumentResolver.setPageParameterName("pageNum");
+        pageableArgumentResolver.setSizeParameterName("pageLimit");
+        argumentResolvers.add(pageableArgumentResolver);
+    }
 }
