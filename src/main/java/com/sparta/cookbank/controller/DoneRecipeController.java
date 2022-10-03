9f1fa8f1465255fdb363865539a3dde6fa1ca017
Rzreo <zuzu5313@naver.com>
2022-10-03 16:37:13 +0900

    [Rzreo] <fix> 주석 추가 + 안쓰는 코드 정리 1

--- src/main/java/com/sparta/cookbank/controller/DoneRecipeController.java
+++ src/main/java/com/sparta/cookbank/controller/DoneRecipeController.java
@@ -15,34 +15,25 @@ public class DoneRecipeController {
     private final DoneRecipeService doneRecipeService;
 
 
-    @PostMapping("/api/recipe/finish")
+    @PostMapping("/api/recipe/finish") //레시피 완료
     public ResponseDto<?> RecipeDone(@RequestParam Long id, @RequestBody DoneRecipeRequestDto requestDto){
         doneRecipeService.UsedIngredient(id,requestDto);
         return ResponseDto.success(id,"나의 레시피 저장소에 저장되었습니다.");
     }
 
-    @PostMapping("/api/recipe/fix")
-    public ResponseDto<?> RecipeFix(@RequestParam Long id, @RequestBody RecipeFixRequestDto requestDto){
-        RecipeFixResponseDto responseDto = doneRecipeService.FixRecipe(id,requestDto);
-        return ResponseDto.success(responseDto,"다음 레시피 정보");
-    }
-
-    // 탄단지나 통계
-    @PostMapping("/api/statistics/ratio/nutrients")
+    @PostMapping("/api/statistics/ratio/nutrients") // 탄단지나 통계
     public ResponseDto<?> getNutrientsRatio(@RequestBody NutrientsRatioRequestDto requestDto ) {
         RatioResponseDto nutrientsRatio = doneRecipeService.getNutrientsRatio(requestDto);
         return ResponseDto.success(nutrientsRatio, "통계자료 제공에 성공하였습니다.");
     }
 
-    // 열량 통계
-    @PostMapping("/api/statistics/ratio/calories")
+    @PostMapping("/api/statistics/ratio/calories")  // 열량 통계
     public ResponseDto<?> getCaloriesRatio(@RequestBody CaloriesRatioRequestDto requestDto) {
         RatioResponseDto caloriesRatio = doneRecipeService.getCaloriesRatio(requestDto);
         return ResponseDto.success(caloriesRatio, "통계자료 제공에 성공하였습니다.");
     }
 
-    //어제 대비 오늘 데이터 조회 통계
-    @GetMapping("/api/statistics/daily")
+    @GetMapping("/api/statistics/daily") //어제 대비 오늘 데이터 조회 통계
     public ResponseDto<?> getDailyRatio() {
         RatioResponseDto dailyRatio = doneRecipeService.getDailyRatio();
         return ResponseDto.success(dailyRatio, "통계자료 제공에 성공하였습니다.");
